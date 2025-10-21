8bf3472e6e1bc338eb927e114afc2b8ad3461b45
Anton Nachodka <krygant8484@gmail.com>
2025-10-21 15:51:13 +1000

    Threads asinc

--- app/src/main/java/ru/netology/nmedia/repository/PostRepository.kt
+++ app/src/main/java/ru/netology/nmedia/repository/PostRepository.kt
@@ -1,12 +1,31 @@
 package ru.netology.nmedia.repository
 
-import androidx.lifecycle.LiveData
 import ru.netology.nmedia.dto.Post
 
 interface PostRepository {
-    fun getAll(): List<Post>
-    fun save(post: Post)
-    fun removeById(id: Long)
-    fun toggleLikeById(id: Long, addLike: Boolean): Post
+    fun save(post: Post, callback: SaveCallback)
+    interface SaveCallback {
+        fun onSuccess(savedPost: Post?)
+        fun onError(e: Exception)
+    }
+
+    fun removeById(id: Long, callback: RemoveCallback)
+
+    interface RemoveCallback {
+        fun onSuccess()
+        fun onError(e: Exception)
+    }
+
+    fun toggleLikeById(id: Long, addLike: Boolean, callback: ToggleLikeCallback)
+    interface ToggleLikeCallback {
+        fun onSuccess(updatedPost: Post?)
+        fun onError(exception: Exception)
+    }
+
+    fun getAllAsync(callback: GetAllCallback)
+    interface GetAllCallback {
+        fun onSuccess(posts: List<Post>) {}
+        fun onError(e: Exception) {}
+    }
 }
 
--- app/src/main/java/ru/netology/nmedia/viewmodel/PostViewModel.kt
+++ app/src/main/java/ru/netology/nmedia/viewmodel/PostViewModel.kt
@@ -1,13 +1,14 @@
 package ru.netology.nmedia.viewmodel
 
 import android.app.Application
-import androidx.lifecycle.*
+import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import ru.netology.nmedia.dto.Post
 import ru.netology.nmedia.model.FeedModel
-import ru.netology.nmedia.repository.*
+import ru.netology.nmedia.repository.PostRepository
+import ru.netology.nmedia.repository.PostRepositoryImpl
 import ru.netology.nmedia.util.SingleLiveEvent
-import java.io.IOException
-import kotlin.concurrent.thread
 
 private val empty = Post(
     id = 0,
@@ -34,28 +35,33 @@ class PostViewModel(application: Application) : AndroidViewModel(application) {
     }
 
     fun loadPosts() {
-        thread {
-            // Начинаем загрузку
-            _data.postValue(FeedModel(loading = true))
-            try {
-                // Данные успешно получены
-                val posts = repository.getAll()
-                FeedModel(posts = posts, empty = posts.isEmpty())
-            } catch (e: IOException) {
-                // Получена ошибка
-                FeedModel(error = true)
-            }.also(_data::postValue)
-        }
+        _data.value = FeedModel(loading = true)
+        repository.getAllAsync(object : PostRepository.GetAllCallback {
+            override fun onSuccess(posts: List<Post>) {
+                _data.postValue(FeedModel(posts = posts, empty = posts.isEmpty()))
+            }
+
+            override fun onError(e: Exception) {
+                _data.postValue(FeedModel(error = true))
+            }
+        })
     }
 
     fun save() {
-        edited.value?.let {
-            thread {
-                repository.save(it)
-                _postCreated.postValue(Unit)
-            }
+        edited.value?.let { post ->
+            repository.save(post, object : PostRepository.SaveCallback {
+                override fun onSuccess(savedPost: Post?) {
+                    if (savedPost != null) {
+                        _postCreated.postValue(Unit)
+                    }
+                }
+
+                override fun onError(e: Exception) {
+                    // обработка ошибки
+                }
+            })
+            edited.value = empty
         }
-        edited.value = empty
     }
 
     fun edit(post: Post) {
@@ -71,29 +77,40 @@ class PostViewModel(application: Application) : AndroidViewModel(application) {
     }
 
     fun removeById(id: Long) {
-        thread {
-            // Оптимистичная модель
-            val old = _data.value?.posts.orEmpty()
-            _data.postValue(
-                _data.value?.copy(posts = _data.value?.posts.orEmpty()
-                    .filter { it.id != id }
-                )
-            )
-            try {
-                repository.removeById(id)
-            } catch (e: IOException) {
-                _data.postValue(_data.value?.copy(posts = old))
+        repository.removeById(id, object : PostRepository.RemoveCallback {
+            override fun onSuccess() {
+                val currentPosts = _data.value?.posts.orEmpty()
+                val indexOfRemovedPost = currentPosts.indexOfFirst { it.id == id }
+
+                if (indexOfRemovedPost >= 0) {
+                    // Локальное обновление списка после успешного удаления
+                    val updatedPosts = currentPosts.toMutableList().apply { removeAt(indexOfRemovedPost) }
+                    _data.postValue(_data.value?.copy(posts = updatedPosts))
+                }
             }
-        }
+
+            override fun onError(e: Exception) {
+                // Обработка ошибки удаления
+            }
+        })
     }
+
     fun toggleLikeById(id: Long) {
-        thread {
-            val currentPosts = _data.value?.posts.orEmpty()
-            val postToUpdate = currentPosts.find { it.id == id }!!
-            val updatedPost = repository.toggleLikeById(id, !postToUpdate.likedByMe)
-            val newPosts = currentPosts.map { if (it.id == id) updatedPost else it }
-            _data.postValue(_data.value?.copy(posts = newPosts))
-        }
+        val currentPosts = _data.value?.posts.orEmpty()
+        val postToUpdate = currentPosts.find { it.id == id } ?: return
+        val updatedPost = repository.toggleLikeById(id, !postToUpdate.likedByMe, object :
+            PostRepository.ToggleLikeCallback {
+            override fun onSuccess(updatedPost: Post?) {
+                if (updatedPost != null) {
+                    val newPosts = currentPosts.map { if (it.id == id) updatedPost else it }
+                    _data.postValue(_data.value?.copy(posts = newPosts))
+                }
+            }
+
+            override fun onError(exception: Exception) {
+                // обработка ошибок
+            }
+        })
     }
 }
 
